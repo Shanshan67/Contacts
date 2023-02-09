f77546f46126f7d7d11d7a2ad56b5475fbfefa88
sorya <sorya@DESKTOP-IDAP76P>
2023-02-09 22:22:52 +1100

    change

---
+++ ContactsAPI/Data/ContactsAPIDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ContactsAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactsAPI.Data
+{
+    public class ContactsAPIDbContext : DbContext
+    {
+        public ContactsAPIDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        //in the db context we have to create properties act as tables
+        public DbSet<Contact> Contacts { get; set; }//because only have on model;dbset property of type contact;and call property contacts
+    }
+}
---
+++ ContactsAPI/Models/AddContactRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace ContactsAPI.Models
+{
+    public class AddContactRequest//to get only certain fields from store it in the database
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public long Phone { get; set; }
+        public string Address { get; set; }
+    }
+}
